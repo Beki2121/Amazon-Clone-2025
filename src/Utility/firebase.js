36ba5aa8b94e454f9a9d5052b3231558aa8114fb
Beki2121 <berekethabtamu2121@gmail.com>
2025-05-28 19:16:16 +0300

    hiding

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -4,16 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCepe_GYjBSdA0dOzegZVkFjrxj5fn2pMM",
-  authDomain: "clone-9c464.firebaseapp.com",
-  projectId: "clone-9c464",
-  storageBucket: "clone-9c464.appspot.com", // fixed typo here too
-  messagingSenderId: "1002860766810",
-  appId: "1:1002860766810:web:e890f65dad78917e905a63",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
